6a99a5301d548017c7633966c972101e81459400
kezie14 <kezie.stewart@outlook.com>
2022-12-29 20:42:52 -0700

    added PORT

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
-const express = requrie("express");
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const path = require("path");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+app.get("/", (req,res) => {
+    res.sendFile(path.join(__dirname, "index.html"))
+});
 
 app.listen(PORT, () => {
     console.log("listening on" + PORT);
